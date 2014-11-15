72eb2ea3941ba6b42a74a846b582842075ad5e25
Jiawei <jiawei.kuang0818@gmail.com>
2014-11-15 00:12:18 -0500

    add convertNumToOrdinal function to NumUtil class

---
+++ src/main/java/jiawei/kuang/common/exception/InvalidIntegerException.java
@@ -0,0 +1,16 @@
+package jiawei.kuang.common.exception;
+
+public class InvalidIntegerException extends RuntimeException {
+
+    
+    /**
+     * @author Jiawei Kuang
+     * @since Nov. 14, 2014
+     */
+    private static final long serialVersionUID = 6800406165081582695L;
+
+    public InvalidIntegerException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/jiawei/kuang/common/util/NumUtil.java
@@ -0,0 +1,26 @@
+package jiawei.kuang.common.util;
+
+import jiawei.kuang.common.exception.InvalidIntegerException;
+
+public class NumUtil {
+    
+    public String convertNumToOrdinal(int num) {
+        if (num <= 0) {
+            throw new InvalidIntegerException("Input number is not positive, so cannot parse to ordinal.");
+        }
+        
+        StringBuffer sb = new StringBuffer();
+        sb.append(num);
+        int remain = num % 10;
+        if (remain == 1) {
+            sb.append("st");
+        } else if (remain == 2) {
+            sb.append("nd");
+        } else if (remain == 3) {
+            sb.append("rd");
+        } else {
+            sb.append("th");
+        }
+        return sb.toString();
+    }
+}
