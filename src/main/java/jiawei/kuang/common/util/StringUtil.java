7eeea1fa95daeccf8f0a91c5ef5dbb3f94452b6b
Jiawei <jiawei.kuang0818@gmail.com>
2014-11-20 13:40:56 -0500

    add StringUtil class

--- src/main/java/jiawei/kuang/common/util/StringUtil.java
+++ src/main/java/jiawei/kuang/common/util/StringUtil.java
@@ -1,11 +1,35 @@
 package jiawei.kuang.common.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class StringUtil {
     
-    public String[] splitString(String string, String regex) {
-        String[] strs = string.split(regex);
-        for (String str: strs) {
-            str.trim();
+    /**
+     * This function partitions a String into several subStrings by some
+     * characters in a set. e.g.
+     * Partition "Who are you? I am Peter." into
+     * ["Who are you?", "I am Peter."]
+     * 
+     * @param originalString
+     * @param seperators is a set containing all the separator characters
+     * @return List<String> by partitioning originalString by separators
+     */
+    public List<String> partitionString(String originalString, Set<Character> separators) {
+        List<String> strs = new ArrayList<String>();
+        char[] originalCharArr = originalString.toCharArray();
+        int len = originalString.length();
+        for (int i = 0; i < len; i++) {
+            StringBuffer sb = new StringBuffer();
+            while (i < len - 1 && !separators.contains(originalCharArr[i])) {
+                sb.append(originalCharArr[i++]);
+            }
+            sb.append(originalCharArr[i]);
+            String str = sb.toString().trim();
+            if (!str.isEmpty()) {
+                strs.add(str);
+            }
         }
         return strs;
     }
